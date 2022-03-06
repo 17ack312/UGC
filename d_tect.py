43205d0a3c586651345faba8be0cfb651dcfa507
Rajdeep basu <48894311+17ack312@users.noreply.github.com>
2022-03-06 06:36:00 +0530

    Update d_tect.py

--- d_tect.py
+++ d_tect.py
@@ -11,7 +11,6 @@ out = mod.replace('module', 'out')
 
 flag=int(sys.argv[1])
 ip=sys.argv[2]
-python=sys.argv[3]
 
 try:
     _rmdir(mod+'d_tect/')
@@ -22,7 +21,7 @@ if not os.path.exists(mod+'d_tect/'):
     os.system('cd "'+mod+'" && git clone https://github.com/17ack312/d_tect.git "'+mod+'d_tect/" --quiet')
 
 try:
-    res=os.popen(python+' "'+mod+'d_tect/d-tect.py" '+str(flag)+' "'+ip+'"').read()
+    res=os.popen('python2 "'+mod+'d_tect/d-tect.py" '+str(flag)+' "'+ip+'"').read()
 except:
     res=None
 
