29b320c274a8121dfad94ff25e884a6fedb32857
Rajdeep basu <48894311+17ack312@users.noreply.github.com>
2022-03-06 06:32:17 +0530

    Create d_tect.py

---
+++ d_tect.py
@@ -0,0 +1,29 @@
+import requests,sys,os,subprocess,tempfile
+
+
+def _rmdir(path):
+    if (os.path.exists(path)):
+        shutil.rmtree(path)
+
+path = tempfile.gettempdir().replace('\\', '/') + '/newxp/'
+mod = path + 'module/'
+out = mod.replace('module', 'out')
+
+flag=int(sys.argv[1])
+ip=sys.argv[2]
+python=sys.argv[3]
+
+try:
+    _rmdir(mod+'d_tect/')
+except:
+    pass
+
+if not os.path.exists(mod+'d_tect/'):
+    os.system('cd "'+mod+'" && git clone https://github.com/17ack312/d_tect.git "'+mod+'d_tect/" --quiet')
+
+try:
+    res=os.popen(python+' "'+mod+'d_tect/d-tect.py" '+str(flag)+' "'+ip+'"').read()
+except:
+    res=None
+
+print(res)
