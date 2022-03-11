a20d6537ef3f95df494336ebbb2d3ffa80380a8e
Rajdeep basu <48894311+17ack312@users.noreply.github.com>
2022-03-11 23:19:36 +0530

    Create host_info.py

---
+++ host_info.py
@@ -0,0 +1,36 @@
+import sys
+import nmap
+nm = nmap.PortScanner()
+
+def _scan(ip):
+	x={}
+	res = nm.scan(hosts=ip, arguments='-sn')['scan']
+	for i in res.keys():
+		res=res[i]
+
+		try:
+			state=str(res['status']['state']).lower()
+		except:
+			state='down'
+		names=[]
+		if state=='up':
+			ip=str(res['addresses']['ipv4'])
+			try:
+				mac=str(res['addresses']['mac'])
+			except:
+				mac=''
+			for j in res['hostnames']:
+				names.append(str(j['name']))
+			x['ip']=ip
+			x['mac']=mac
+			x['name']=names
+	return x
+
+host=sys.argv[1]
+data={}
+for i in host.split('##'):
+	x=(_scan(i))
+	if int(len(x.keys()))>0:
+		data[i]=x
+
+print(data)
