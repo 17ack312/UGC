f344a54db7683e3ab345a4b6fe1259ab37ca6252
Rajdeep basu <48894311+17ack312@users.noreply.github.com>
2022-03-13 02:18:26 +0530

    Update _ssl.py

--- _ssl.py
+++ _ssl.py
@@ -1,5 +1,5 @@
 import os,sys,nmap
-import re ,datetime
+import re ,datetime,json
 #import subprocess
 
 nm=nmap.PortScanner()
@@ -54,8 +54,8 @@ def process_data(data):
                                score=5.3
                                strng='CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:N/I:L/A:N'
                                risk='Medium'
-                               desc="The remote server's SSL certificate has already expired."
-                               imp="When you have an SSL certificate properly installed, your website’s server will engage in something called the SSL handshake anytime a visitor wants to make a connection. During this handshake, the user’s browser will be presented with the site’s SSL certificate. It needs to authenticate the certificate in order to complete the handshake.The authentication process requires the certificate to be within its validity dates. Every certificate has an issued and expired data coded into it. This allows the browser to determine whether it’s still valid or has expired. If the certificate is expired, the user’s browser has no way to validate the server. That means it can’t definitively tell you if the website presenting this certificate is its rightful owner.That’s going to cause a browser error that says your connection is not secure. The error is big; it blocks people from getting to your website – effectively breaking the site.Now, depending on how you’ve configured your server — all hope may not be lost. But you’d have to advise your customers to click through a browser warning, which most people aren’t going to do.However, if you’ve set up your website to use HTTP Strict Transport Security (HSTS), clicking through the warning won’t even be an option. HSTS forces secure connections, and if the certificate isn’t valid, the browser won’t be able to make one. In that case, your website is completely broken."
+                               desc="The SSL certificate has already expired."
+                               imp="When you have an SSL certificate properly installed, your server will engage in something called the SSL handshake anytime a visitor wants to make a connection. During this handshake, the user’s browser will be presented with the site’s SSL certificate. It needs to authenticate the certificate in order to complete the handshake.The authentication process requires the certificate to be within its validity dates. Every certificate has an issued and expired data coded into it. This allows the browser to determine whether it’s still valid or has expired. If the certificate is expired, the user’s browser has no way to validate the server. That means it can’t definitively tell you if the website presenting this certificate is its rightful owner.That’s going to cause a browser error that says your connection is not secure. The error is big; it blocks people from getting to your website – effectively breaking the site.Now, depending on how you’ve configured your server — all hope may not be lost. But you’d have to advise your customers to click through a browser warning, which most people aren’t going to do.However, if you’ve set up your website to use HTTP Strict Transport Security (HSTS), clicking through the warning won’t even be an option. HSTS forces secure connections, and if the certificate isn’t valid, the browser won’t be able to make one. In that case, your website is completely broken."
                                sol='Purchase or generate a new SSL certificate to replace the existing one.'
                                ref=''
                                link=''
@@ -133,7 +133,7 @@ def process_data(data):
                        strng='CVSS:3.0/AV:N/AC:H/PR:N/UI:N/S:U/C:H/I:N/A:N'
                        risk='Medium'
                        desc='The remote host supports SSLv2 and therefore may be affected by a vulnerability that allows a cross-protocol Bleichenbacher padding oracle attack known as DROWN (Decrypting RSA with Obsolete and Weakened eNcryption).'
-                       imp="This vulnerability exists due to a flaw in the Secure Sockets Layer Version 2 (SSLv2) implementation, and it allows captured TLS traffic to be decrypted. A man-in-the-middle attacker can exploit this to decrypt the TLS connection by utilizing previously captured traffic and weak cryptography along with a series of specially crafted connections to an SSLv2 server that uses the same private key.The SSLv2 protocol, as used in OpenSSL before 1.0.1s and 1.0.2 before 1.0.2g and other products, requires a server to send a ServerVerify message before establishing that a client possesses certain plaintext RSA data, which makes it easier for remote attackers to decrypt TLS ciphertext data by leveraging a Bleichenbacher RSA padding oracle, aka a \"DROWN\" attack."
+                       imp="This vulnerability exists due to a flaw in the Secure Sockets Layer Version 2 (SSLv2) implementation, and it allows captured TLS traffic to be decrypted. A man-in-the-middle attacker can exploit this to decrypt the TLS connection by utilizing previously captured traffic and weak cryptography along with a series of specially crafted connections to an SSLv2 server that uses the same private key.The SSLv2 protocol, as used in OpenSSL before 1.0.1s and 1.0.2 before 1.0.2g and other products, requires a server to send a ServerVerify message before establishing that a client possesses certain plaintext RSA data, which makes it easier for remote attackers to decrypt TLS ciphertext data by leveraging a Bleichenbacher RSA padding oracle, aka a DROWN attack."
                        sol='Disable SSLv2 and export grade cryptography cipher suites. Ensure that private keys are not used anywhere with server software that supports SSLv2 connections.'
                        ref='CVE-2016-0800,CERT:583776'
                        link='https://drownattack.com/,https://drownattack.com/drown-attack-paper.pdf'
@@ -224,7 +224,7 @@ def process_data(data):
                        strng='CVSS:3.0/AV:N/AC:H/PR:N/UI:N/S:C/C:H/I:N/A:N'
                        risk='Medium'
                        desc='It is possible to obtain sensitive information from the remote host with SSL/TLS-enabled services.'
-                       imp='The remote host is affected by a man-in-the-middle (MitM) information disclosure vulnerability known as POODLE. The vulnerability is due to the way SSL 3.0 handles padding bytes when decrypting messages encrypted using block ciphers in cipher block chaining (CBC) mode.MitM attackers can decrypt a selected byte of a cipher text in as few as 256 tries if they are able to force a victim application to repeatedly send the same data over newly created SSL 3.0 connections.As long as a client and service both support SSLv3, a connection can be \'rolled back\' to SSLv3, even if TLSv1 or newer is supported by the client and service.The TLS Fallback SCSV mechanism prevents \'version rollback\' attacks without impacting legacy clients; however, it can only protect connections when the client and service support the mechanism. Sites that cannot disable SSLv3 immediately should enable this mechanism.This is a vulnerability in the SSLv3 specification, not in any particular SSL implementation. Disabling SSLv3 is the only way to completely mitigate the vulnerability.'
+                       imp='The remote host is affected by a man-in-the-middle (MitM) information disclosure vulnerability known as POODLE. The vulnerability is due to the way SSL 3.0 handles padding bytes when decrypting messages encrypted using block ciphers in cipher block chaining (CBC) mode.MitM attackers can decrypt a selected byte of a cipher text in as few as 256 tries if they are able to force a victim application to repeatedly send the same data over newly created SSL 3.0 connections.As long as a client and service both support SSLv3, a connection can be rolled back to SSLv3, even if TLSv1 or newer is supported by the client and service.The TLS Fallback SCSV mechanism prevents version rollback attacks without impacting legacy clients; however, it can only protect connections when the client and service support the mechanism. Sites that cannot disable SSLv3 immediately should enable this mechanism.This is a vulnerability in the SSLv3 specification, not in any particular SSL implementation. Disabling SSLv3 is the only way to completely mitigate the vulnerability.'
                        sol='Disable SSLv3.Services that must support SSLv3 should enable the TLS Fallback SCSV mechanism until SSLv3 can be disabled.'
                        ref='CVE-2014-3566,CERT:577193'
                        link='https://www.imperialviolet.org/2014/10/14/poodle.html,https://www.openssl.org/~bodo/ssl-poodle.pdf.https://tools.ietf.org/html/draft-ietf-tls-downgrade-scsv-00'
@@ -263,7 +263,7 @@ def _sslyze():
                     strng = 'CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:N/I:L/A:N'
                     risk = 'Medium'
                     desc = 'The SSL certificate for this service is for a different host.'
-                    imp = 'The \'commonName\' (CN) attribute of the SSL certificate presented for this service is for a different machine.'
+                    imp = 'The commonName (CN) attribute of the SSL certificate presented for this service is for a different machine.'
                     sol = 'Purchase or generate a proper SSL certificate for this service.'
                     ref = 'CWE-297'
                     link = 'https://help.ucybersolutions.com/index.php/2021/09/09/how-to-solve-ssl-certificate-with-wrong-hostname-error/'
@@ -291,7 +291,7 @@ def _sslyze():
                     strng='CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:L/I:L/A:N'
                     risk='Medium'
                     desc='The SSL certificate for this service cannot be trusted.'
-                    imp='''The server's X.509 certificate cannot be trusted. This situation can occur in three different ways, in which the chain of trust can be broken, as stated below :\n- First, the top of the certificate chain sent by the server might not be descended from a known public certificate authority. This can occur either when the top of the chain is an unrecognized, self-signed certificate, or when intermediate certificates are missing that would connect the top of the certificate chain to a known public certificate authority.\n- Second, the certificate chain may contain a certificate that is not valid at the time of the scan. This can occur either when the scan occurs before one of the certificate's 'notBefore' dates, or after one of the certificate's 'notAfter' dates.\n- Third, the certificate chain may contain a signature that either didn't match the certificate's information or could not be verified. Bad signatures can be fixed by getting the certificate with the bad signature to be re-signed by its issuer. Signatures that could not be verified are the result of the certificate's issuer using a signing algorithm that Nessus either does not support or does not recognize.\nIf the remote host is a public host in production, any break in the chain makes it more difficult for users to verify the authenticity and identity of the web server. This could make it easier to carry out man-in-the-middle attacks against the remote host.'''
+                    imp='The X.509 certificate cannot be trusted. This situation can occur in three different ways, in which the chain of trust can be broken, as stated below :\n- First, the top of the certificate chain sent by the server might not be descended from a known public certificate authority. This can occur either when the top of the chain is an unrecognized, self-signed certificate, or when intermediate certificates are missing that would connect the top of the certificate chain to a known public certificate authority.\n- Second, the certificate chain may contain a certificate that is not valid at the time of the scan. This can occur either when the scan occurs before one of the certificate notBefore dates, or after one of the certificates notAfter dates.\n- Third, the certificate chain may contain a signature that either did not match the certificate information or could not be verified. Bad signatures can be fixed by getting the certificate with the bad signature to be re-signed by its issuer.\nIf the remote host is a public host in production, any break in the chain makes it more difficult for users to verify the authenticity and identity of the web server. This could make it easier to carry out man-in-the-middle attacks against the remote host.'
                     sol='Purchase or generate a proper SSL certificate for this service.'
                     ref='CVE-2020-35733'
                     link='https://www.itu.int/rec/T-REC-X.509/en,https://en.wikipedia.org/wiki/X.509'
@@ -339,7 +339,7 @@ def _sslyze():
                     strng = 'CVSS:3.0/AV:N/AC:H/PR:N/UI:N/S:C/C:H/I:N/A:N'
                     risk = 'Medium'
                     desc = 'It is possible to obtain sensitive information from the remote host with SSL/TLS-enabled services.'
-                    imp = 'The remote host is affected by a man-in-the-middle (MitM) information disclosure vulnerability known as POODLE. The vulnerability is due to the way SSL 3.0 handles padding bytes when decrypting messages encrypted using block ciphers in cipher block chaining (CBC) mode.MitM attackers can decrypt a selected byte of a cipher text in as few as 256 tries if they are able to force a victim application to repeatedly send the same data over newly created SSL 3.0 connections.As long as a client and service both support SSLv3, a connection can be \'rolled back\' to SSLv3, even if TLSv1 or newer is supported by the client and service.The TLS Fallback SCSV mechanism prevents \'version rollback\' attacks without impacting legacy clients; however, it can only protect connections when the client and service support the mechanism. Sites that cannot disable SSLv3 immediately should enable this mechanism.This is a vulnerability in the SSLv3 specification, not in any particular SSL implementation. Disabling SSLv3 is the only way to completely mitigate the vulnerability.'
+                    imp = 'The remote host is affected by a man-in-the-middle (MitM) information disclosure vulnerability known as POODLE. The vulnerability is due to the way SSL 3.0 handles padding bytes when decrypting messages encrypted using block ciphers in cipher block chaining (CBC) mode.MitM attackers can decrypt a selected byte of a cipher text in as few as 256 tries if they are able to force a victim application to repeatedly send the same data over newly created SSL 3.0 connections.As long as a client and service both support SSLv3, a connection can be rolled back to SSLv3, even if TLSv1 or newer is supported by the client and service.The TLS Fallback SCSV mechanism prevents version rollback attacks without impacting legacy clients; however, it can only protect connections when the client and service support the mechanism. Sites that cannot disable SSLv3 immediately should enable this mechanism.This is a vulnerability in the SSLv3 specification, not in any particular SSL implementation. Disabling SSLv3 is the only way to completely mitigate the vulnerability.'
                     sol = 'Disable SSLv3.Services that must support SSLv3 should enable the TLS Fallback SCSV mechanism until SSLv3 can be disabled.'
                     ref = 'CVE-2014-3566,CERT:577193'
                     link = 'https://www.imperialviolet.org/2014/10/14/poodle.html,https://www.openssl.org/~bodo/ssl-poodle.pdf.https://tools.ietf.org/html/draft-ietf-tls-downgrade-scsv-00'
@@ -350,14 +350,22 @@ def _sslyze():
 _ssl_enum()
 _sslyze()
 
-print(result)
 
 """
 res = sorted(result.items(), key = lambda x: x[1]['score'],reverse=True)
 
 result={}
 for i in res:
-  result[i[0]]=dict(i[1])
+  x=i[0]
+  y=dict(i[1])
+  result[x]=y
+
+print(dict(result))
+"""
+
+#or i in result:
+#  print(result[i])
+
+result=(str(result).replace("'",'"'))
 
 print(result)
-"""
