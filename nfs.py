92bfddbe41cd438de25cfe91cd756e634db0abb0
Rajdeep basu <48894311+17ack312@users.noreply.github.com>
2022-04-03 23:33:25 +0530

    Create nfs.py

---
+++ nfs.py
@@ -0,0 +1,92 @@
+import os,sys,nmap
+import re ,datetime,json
+#import subprocess
+
+nm=nmap.PortScanner()
+#python=sys.argv[2]
+
+result={}
+ports=[]
+host=sys.argv[1]
+
+def _scan(ip,arg):
+    res=nm.scan(hosts=ip,arguments=arg)['scan']
+    for i in res.keys():
+        res=res[i]
+    return res
+
+def set_data(v_name,score,strng,risk,desc,imp,sol,ref,link,port,script,serv):
+    vuln={}
+
+    vuln['name'] = str(v_name)
+    vuln['score'] = float(score)
+    vuln['string'] = str(strng)
+    vuln['risk'] = str(risk)
+    vuln['desc'] = str(desc)
+    vuln['imp'] = str(imp)
+    vuln['sol'] = str(sol)
+    vuln['ref'] = str(ref)
+    vuln['link'] = str(link)
+    vuln['port']=str(port)
+    vuln['service']=str(serv)
+    vuln['output']=str(script)
+
+    return vuln
+
+def process_data(data):
+   for i in data.keys():
+       if 'script' in (data[i].keys()) and str(data[i]['state'])=='open':
+           port=str(i)
+           ports.append(port)
+           name=str(data[i]['name'])
+
+           for j in (data[i]['script'].keys()):
+              script=data[i]['script'][j]
+
+
+              if str(j)=='telnet-encryption' and not re.search('Telnet server supports encryption',script,re.IGNORECASE):
+                v_name='Unencrypted Telnet Server'
+                score=
+                strng='CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:L/I:L/A:N'
+                risk='Medium'
+                desc='The remote host is running a Telnet server over an unencrypted channel.The remote Telnet server transmits traffic in cleartext.'
+                imp='Using Telnet over an unencrypted channel is not recommended as logins, passwords, and commands are transferred in cleartext. This allows a remote, man-in-the-middle attacker to eavesdrop on a Telnet session to obtain credentials or other sensitive information and to modify traffic exchanged between a client and server.SSH is preferred over Telnet since it protects credentials from eavesdropping and can tunnel additional data streams such as an X11 session.'
+                sol='Disable the Telnet service and use SSH instead.'
+                ref=''
+                link=''
+
+                head=' [MED] UNENCRYPTED TELNET SERVER'
+                result[head]=set_data(v_name,score,strng,risk,desc,imp,sol,ref,link,port,script,name)
+
+  
+
+def _nfs_enum():
+    res=_scan(host,'-sV --script=telnet-encryption.nse -F')
+
+    if 'tcp' in res.keys():
+        data=res['tcp']
+        process_data(data)
+    if 'udp' in res.keys():
+        data=res['udp']
+        process_data(data)
+
+_nfs_enum()
+
+
+res = sorted(result.items(), key = lambda x: x[1]['score'],reverse=True)
+
+result={}
+for i in res:
+  x=i[0]
+  y=dict(i[1])
+  result[x]=y
+
+#print(dict(result))
+
+
+#or i in result:
+#  print(result[i])
+
+#result=(str(result).replace("'",'"'))
+
+print(json.dumps(result))
